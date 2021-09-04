6b8790d94dae7ccb81c810ba294a815819276ab8
Kader Oral <63172521+tutunamayanlar2021@users.noreply.github.com>
2021-09-04 13:09:41 +0000

    variables

---
+++ main.dart
@@ -0,0 +1,9 @@
+void main() {
+String name ="kader";
+int year = 2000;
+bool isLogin=true; 
+double rate =9.9;
+print(name +" "+ year.toString());
+print("Login : "+ isLogin.toString());
+print("Rate : "+ rate.toString());
+}
